dadb7a6054517de0677d9231f27e9fa3e4c52118
jacobl3371 <jacobl161@nycstudents.net>
2025-03-22 16:25:34 -0400

    add try/catch error handling to express backend

--- backend/api/index.js
+++ backend/api/index.js
@@ -7,19 +7,24 @@ const app = express()
 app.use(cors())
 
 app.get("/api/fetchlanguage", async (req, res) => {
-    const ip = req.ip
+    try {
+        const ip = req.ip
 
-    if (ip === "127.0.0.1" || ip === "::1") {
-        return next()
-    }
+        if (ip === "127.0.0.1" || ip === "::1") {
+            return next()
+        }
 
-    const apiUrl = `https://freeipapi.com/api/json/${ip}`
-    const response = await fetch(apiUrl)
-    if (!response.ok) {
-        throw new Error(`API responded with status ${response.status}`)
+        const apiUrl = `https://freeipapi.com/api/json/${ip}`
+        const response = await fetch(apiUrl)
+        if (!response.ok) {
+            throw new Error(`API responded with status ${response.status}`)
+        }
+        const data = await response.json()
+        return res.json({ language: data.countryCode.toLowerCase()})
     }
-    const data = await response.json()
-    return res.json({ language: data.countryCode.toLowerCase()})
+    catch {
+        return res.json({ error: error.message })
+    }
 })
 
 app.listen(3000)
