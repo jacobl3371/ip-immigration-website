86aa04f4d79a47461a4f9a1ec3c216fe6b20dd91
jacobl3371 <jacobl161@nycstudents.net>
2025-03-23 23:38:57 -0400

    fix default language fallback handling

--- frontend/src/initI18n.js
+++ frontend/src/initI18n.js
@@ -1,15 +1,21 @@
 import i18next from "i18next"
 import { initReactI18next } from "react-i18next"
+import defaultLang from "./locales/us.json"
 
 const initI18n = async () => {
     i18next.use(initReactI18next).init({
-        fallbackLng: "en"
+        fallbackLng: "us"
     })
+    i18next.addResourceBundle("us", "translation", defaultLang)
 
     try {
         const response = await fetch("http://localhost:3000/api/fetchlanguage")
         const data = await response.json()
         const lang = data.language
+
+        if(lang==="us"){
+            return
+        } 
 
         const module = await import(`./locales/${lang}.json`)
         i18next.addResourceBundle(lang, "translation", module.default || module)
