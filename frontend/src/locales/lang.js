abb85f41db0cb00699ccfbf7dede3dc334217301
jacobl3371 <jacobl161@nycstudents.net>
2025-05-25 22:46:38 -0400

    added all langauges

--- frontend/src/locales/lang.js
+++ frontend/src/locales/lang.js
@@ -11,4 +11,104 @@ export const languages = [
     code: "es",
     name: "Español",
   },
+  {
+    code: "it",
+    name: "Italiano",
+  },
+  {
+    code: "pt",
+    name: "Português",
+  },
+  {
+    code: "ru",
+    name: "Русский",
+  },
+  {
+    code: "zh",
+    name: "中國人",
+  },
+  {
+    code: "ar",
+    name: "عربي",
+  },
+  {
+    code: "fr",
+    name: "Français",
+  },
+  {
+    code: "nl",
+    name: "Nederlands",
+  },
+  {
+    code: "sv",
+    name: "Svenska",
+  },
+  {
+    code: "no",
+    name: "norsk",
+  },
+  {
+    code: "da",
+    name: "Dansk",
+  },
+  {
+    code: "fi",
+    name: "suomi",
+  },
+  {
+    code: "hu",
+    name: "magyar",
+  },
+  {
+    code: "po",
+    name: "Polski",
+  },
+  {
+    code: "ja",
+    name: "日本語",
+  },
+  {
+    code: "he",
+    name: "עִברִית",
+  },
+  {
+    code: "uk",
+    name: "українська",
+  },
+  {
+    code: "cs",
+    name: "čeština",
+  },
+  {
+    code: "lv",
+    name: "Latviešu",
+  },
+  {
+    code: "lt",
+    name: "Lietuvių",
+  },
+  {
+    code: "ro",
+    name: "Română",
+  },
+  {
+    code: "sk",
+    name: "Slovák",
+  },
+  {
+    code: "bg",
+    name: "български",
+  },
+  {
+    code: "vi",
+    name: "Tiếng Việt",
+  },
+  {
+    code: "id",
+    name: "Indonesia",
+  },
+  {
+    code: "ms",
+    name: "Melayu",
+  },
 ];
