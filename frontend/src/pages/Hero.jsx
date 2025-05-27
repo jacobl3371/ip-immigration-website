1fc3dfe3aac1797154d9992a43a2f8f632fb42e6
jacobl3371 <jacobl161@nycstudents.net>
2025-05-26 20:30:33 -0400

    adjust background image sizing tailwind

--- frontend/src/pages/Hero.jsx
+++ frontend/src/pages/Hero.jsx
@@ -6,12 +6,12 @@ export default function Hero() {
   const { t } = useTranslation();
 
   return (
-    <div className="relative w-screen h-screen ">
+    <div className="relative w-screen h-screen overflow-hidden">
       <div className="absolute inset-0 bg-black/50 z-10"></div>
 
       <div className="absolute inset-0 bg-[url('/Hero.webp')] bg-cover h-full bg-center bg-no-repeat z-0"></div>
 
-      <div className="relative z-20 h-full">
+      <div className="relative z-20 h-full flex flex-col">
         <Navbar />
         <div className="h-full w-full flex justify-center flex-col mx-16">
           <h1 className="text-white text-4xl max-w-[400px]">
